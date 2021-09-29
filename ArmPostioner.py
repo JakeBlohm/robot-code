2a52fc9ce292dc32e587e7de117ebae272c2bc9b
JakeBlohm <91258279+JakeBlohm@users.noreply.github.com>
2021-09-29 14:58:50 +0100

    jf
    fe

---
+++ ArmPostioner.py
@@ -0,0 +1,18 @@
+# Input: Coords + arm geo + joint limits
+# Return: motor targetAngle
+
+# Motor Offsets 
+# 'Horizontal' Offsets
+MOTOR_TWO_OFFSET = 0
+MOTOR_THREE_OFFSET = 0
+MOTOR_FOUR_OFFSET = 0
+
+#'vertical' Offset
+MOTOR_ONE_OFFSET = 0
+MOTOR_FIVE_OFFSET = 0
+GRIPPER_OFFSET = 0
+
+#Segments
+SEGMENT_ONE = 30
+
+
