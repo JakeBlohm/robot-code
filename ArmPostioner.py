196eb9e77c6afaeed4b19cc3dbf14e13bfe7a036
JakeBlohm <91258279+JakeBlohm@users.noreply.github.com>
2021-09-29 20:38:24 +0100

    Global the stuff in armpos and added the input
    not much but slowly

--- ArmPostioner.py
+++ ArmPostioner.py
@@ -1,11 +1,23 @@
 # Input: Coords + arm geo + joint limits
 # Return: motor targetAngle
 
+#GLOBAL is good
+global MOTOR_ONE_OFFSET
+global MOTOR_TWO_OFFSET
+global MOTOR_THREE_OFFSET
+global MOTOR_FOUR_OFFSET
+global MOTOR_FIVE_OFFSET
+global SEGMENT_ONE
+global SEGMENT_TWO
+global ALL_HORIZONTAL_OFFSET
+
 # Motor Offsets 
 # 'Horizontal' Offsets
 MOTOR_TWO_OFFSET = 0
 MOTOR_THREE_OFFSET = 0
 MOTOR_FOUR_OFFSET = 0
+
+ALL_HORIZONTAL_OFFSET = (MOTOR_TWO_OFFSET + MOTOR_THREE_OFFSET + MOTOR_FOUR_OFFSET)
 
 #'vertical' Offset
 MOTOR_ONE_OFFSET = 0
@@ -14,5 +26,13 @@ GRIPPER_OFFSET = 0
 
 #Segments
 SEGMENT_ONE = 30
+SEGMENT_TWO = 20
 
+def TEMP_Input():
+    Y = int(input("Y"))
+    X = int(input("X"))
+
+#MotorOne position calc
+
+motorOne = ()
 
