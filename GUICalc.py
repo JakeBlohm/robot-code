ad78005f074f03b5d02a8595ac31c325c5f2bc09
JakeBlohm <91258279+JakeBlohm@users.noreply.github.com>
2021-11-05 12:48:38 +0000

    no round

--- GUICalc.py
+++ GUICalc.py
@@ -6,14 +6,15 @@ def getXY(mOneCAngle, mTwoCAngle, mThreeCAngle):
         curDistanceAngled = math.sqrt((SEGMENT_TWO**2)+(SEGMENT_ONE**2)-(2*SEGMENT_TWO*SEGMENT_ONE*math.cos(math.radians(180-mThreeCAngle))))
         AAngle = 90 - ((math.degrees(math.acos(((SEGMENT_ONE**2)+(curDistanceAngled**2)-(SEGMENT_TWO**2))/(2*SEGMENT_ONE*curDistanceAngled))))+mTwoCAngle)
         curDistance = math.cos(math.radians(AAngle))*curDistanceAngled
-        X = round(curDistance*math.sin(math.radians(mOneCAngle)),2)
-        Y = round(curDistance*math.cos(math.radians(mOneCAngle)),2)
-        Z = round(curDistanceAngled*math.sin(math.radians(AAngle)),2)
+        X = curDistance*math.sin(math.radians(mOneCAngle))
+        Y = curDistance*math.cos(math.radians(mOneCAngle))
+        Z = curDistanceAngled*math.sin(math.radians(AAngle))
 
         midDis= SEGMENT_ONE*math.sin(math.radians(mTwoCAngle))
         midX = midDis*math.sin(math.radians(mOneCAngle))
         midY = midDis*math.cos(math.radians(mOneCAngle))
         midZ= SEGMENT_ONE*math.cos(math.radians(mTwoCAngle))
+        print (midZ)
         return X, Y, Z, curDistance, midDis, midX, midY, midZ
     except:
         return 0, 0, 0, 0, 0, 0, 0, 0
