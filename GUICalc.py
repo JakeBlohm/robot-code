d530f65e2aa2e2f43227a21806bbe1b259f5d427
JakeBlohm <91258279+JakeBlohm@users.noreply.github.com>
2021-11-05 15:07:00 +0000

    update faild

--- GUICalc.py
+++ GUICalc.py
@@ -1,7 +1,7 @@
 import math
-from ArmPostioner import SEGMENT_ONE, SEGMENT_TWO
+from ArmPostioner import SEGMENT_ONE, SEGMENT_THREE, SEGMENT_TWO
 
-def getXY(mOneCAngle, mTwoCAngle, mThreeCAngle):
+def getXY(mOneCAngle, mTwoCAngle, mThreeCAngle, mFourCAngle, mFiveCAngle, mSixCAngle):
     try:
         curDistanceAngled = math.sqrt((SEGMENT_TWO**2)+(SEGMENT_ONE**2)-(2*SEGMENT_TWO*SEGMENT_ONE*math.cos(math.radians(180-mThreeCAngle))))
         AAngle = 90 - ((math.degrees(math.acos(((SEGMENT_ONE**2)+(curDistanceAngled**2)-(SEGMENT_TWO**2))/(2*SEGMENT_ONE*curDistanceAngled))))+mTwoCAngle)
@@ -15,10 +15,14 @@ def getXY(mOneCAngle, mTwoCAngle, mThreeCAngle):
         midY = midDis*math.cos(math.radians(mOneCAngle))
         midZ = SEGMENT_ONE*math.cos(math.radians(mTwoCAngle))
         
-        griDis =
-        griX = 
-        griY =
-        griZ = 
+        tempDis = SEGMENT_THREE*math.sin(math.radians(mFiveCAngle))
+        griX = tempDis*math.cos(math.radians(mFourCAngle)) + X
+        griY = SEGMENT_THREE*math.cos(math.radians(mFiveCAngle)) + Y
+        griZ = tempDis*math.sin(math.radians(mFourCAngle)) + Z
+        griDis = math.sqrt((griX**2)+(griY**2)) + curDistance
+
+
+
         return X, Y, Z, curDistance, midDis, midX, midY, midZ, griX, griY, griZ, griDis
     except:
         return 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
